9b1f46b352e88c424ca858e09f9305903bdda07f
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2017-11-23 15:54:52 +1100

    Add sorting to display for animals and sea-critters

--- src/models/animal.js
+++ src/models/animal.js
@@ -19,6 +19,18 @@ let animals = [
 let nextID = 4;
 
 function all() {
+  animals.sort( function (a, b) {
+    let nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    let nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    // names must be equal
+    return 0;
+  })
   return animals;
 }
 
--- src/models/seacritter.js
+++ src/models/seacritter.js
@@ -19,6 +19,18 @@ let seaCritters = [
 let nextId = 4; 
 
 function all() {
+  seaCritters.sort( function (a, b) {
+    let nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    let nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    // names must be equal
+    return 0;
+  })
   return seaCritters;
 }
 
